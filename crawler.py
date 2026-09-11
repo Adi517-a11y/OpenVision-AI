16e4046b4287329fd575339597650332c1083697
Adi517-a11y <Adriantahir17@hotmail.com>
2026-09-11 11:13:29 +0800

    Implement Wikimedia API image URL crawler
    Added functionality to fetch image URLs from Wikimedia API.

--- crawler.py
+++ crawler.py
@@ -1,1 +1,33 @@
-print("OpenVision dataset crawler starting...")
+import requests
+
+API_URL = "https://commons.wikimedia.org/w/api.php"
+
+params = {
+    "action": "query",
+    "format": "json",
+    "generator": "search",
+    "gsrsearch": "cat",
+    "gsrnamespace": 6,
+    "gsrlimit": 5,
+    "prop": "imageinfo",
+    "iiprop": "url"
+}
+
+response = requests.get(
+    API_URL,
+    params=params,
+    timeout=30
+)
+
+response.raise_for_status()
+
+data = response.json()
+
+pages = data.get("query", {}).get("pages", {})
+
+for page in pages.values():
+    image_info = page.get("imageinfo", [{}])[0]
+    url = image_info.get("url")
+
+    if url:
+        print(url)
